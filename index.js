e4e995fcfa1493f290d360b234c35472f71c7216
Wendy Wang <wendywangg928@gmail.com>
2023-07-13 16:39:42 -0400

    updated and installed methodOverride

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 require('dotenv').config()
 const express = require('express')
+const methodOverride = require('method-override')
 const app = express()            //check documentation you get module from to see if it needs to be invoked
+const methodOverrride = require('method-override')
 const PORT=process.env.PORT
 
 app.set('view engine','jsx')
 app.engine('jsx',require('express-react-views').createEngine())
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
 
 app.use('/places',require('./controllers/places'))  //connects any search to /places to the controllers/places file
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,12 +1,46 @@
 const React = require('react')
 const Def = require('../default')
 
-function show(){
-    return(
+function show(data) {
+    return (
         <Def>
             <main>
-                <h1>Show Page</h1>
+                <h1>{data.place.name}</h1>
             </main>
+            <div className='showjsx-maincontainer'>
+                <div className='showjsx-img' >
+                    <img src={data.place.pic} alt='restaurant picture'></img>
+                </div>
+                <div className='showjsx-contentcontainer'>
+                    <div>
+                        <h2>Rating</h2>
+                        <p>Not rated yet! Be the first person!</p>
+                    </div>
+                    <div>
+                        <h2>Description</h2>
+                        <p>Located in {data.place.city}, {data.place.state} and serving {data.place.cuisines}</p>
+                    </div>
+                    <div className='showjsx-editndeletebox'>
+                        <div>
+                            <a href={`/places/${data.id}/edit`} className="btn btn-warning">
+                                Edit
+                            </a>
+                        </div>
+                        <form method='POST' action={`/places/${data.id}?_method=DELETE`}>
+                            <button type="submit" className="btn btn-danger">
+                                Delete
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <hr></hr>
+            <footer>
+                <div className='showjsx-footer'>
+                    <h2>Comments</h2>
+                    <p>No comments yet, be the first!</p>
+                </div>
+            </footer>
         </Def>
     )
 }
