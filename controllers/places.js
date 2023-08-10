a72cc91a3eb030524e97bddb8aa71a492dabb7b3
Wendy Wang <wendywangg928@gmail.com>
2023-08-10 16:05:51 -0400

    finished day 10

--- controllers/places.js
+++ controllers/places.js
@@ -1,97 +1,104 @@
-const router = require('express').Router()
-const db = require('../models/index')
+const router = require("express").Router();
+const db = require("../models/index");
 
-router.get('/',(req,res)=>{
-    db.Place.find()
-    .then((places)=>{
-        res.render('places/index',{places})
+router.get("/", (req, res) => {
+  db.Place.find()
+    .then((places) => {
+      res.render("places/index", { places });
     })
-    .catch(err =>{
-        res.render('error404')
-    })
-})
+    .catch((err) => {
+      res.render("error404");
+    });
+});
 
-
-router.post('/', (req, res) => {
-    db.Place.create(req.body)
+router.post("/", (req, res) => {
+  db.Place.create(req.body)
     .then(() => {
-        res.redirect('/places')
+      res.redirect("/places");
     })
-    .catch(err => {
-        if (err && err.name == 'ValidationError') {
-            let message = 'Validation Error!'
-            // Todo: Find all validation errors
-            res.render('places/new', { message })
-        }
-        else {
-            res.render('error404')
-        }
+    .catch((err) => {
+      if (err && err.name == "ValidationError") {
+        let message = "Validation Error!";
+        // Todo: Find all validation errors
+        res.render("places/new", { message });
+      } else {
+        res.render("error404");
+      }
+    });
+});
+
+router.get("/new", (req, res) => {
+  res.render("places/new");
+});
+
+router.get("/:id", (req, res) => {
+  db.Place.findById(req.params.id)
+    .populate("comments")
+    .then((place) => {
+      console.log(place.comments);
+      res.render("places/show", { place });
     })
-})
-  
+    .catch((err) => {
+      res.render("error404");
+    });
+});
 
-router.get('/new',(req,res)=>{
-   res.render('places/new')
-})
-
-router.get('/:id', (req, res) => {
-    db.Place.findById(req.params.id)
-    .populate('comments')
-    .then(place => {
-        console.log(place.comments)
-        res.render('places/show', { place })
+router.put("/:id", (req, res) => {
+  db.Place.findByIdAndUpdate(req.params.id, req.body)
+    .then(() => {
+      res.redirect(`/places/${req.params.id}`);
     })
-    .catch(err => {
-        res.render('error404')
+    .catch((err) => {
+      res.render("error404");
+    });
+});
+
+router.get("/:id/edit", (req, res) => {
+  db.Place.findById(req.params.id)
+    .then((place) => {
+      res.render("places/edit", { place });
     })
-})
+    .catch((err) => {
+      res.render("error404");
+    });
+});
 
+router.delete("/:id", (req, res) => {
+  db.Place.findByIdAndDelete(req.params.id)
+    .then(() => {
+      res.status(303).redirect("/places");
+    })
+    .catch((err) => {
+      res.render("error404");
+    });
+});
 
-router.put('/:id', (req, res) => {
-    
-})
-  
-  
-
-router.get('/:id/edit',(req,res)=>{
-    
-})
-
-router.delete('/:id',(req,res)=>{
-    db.Place.findByIdAndDelete(req.params.id)
-    .then(()=>{res.status(303).redirect('/places')})
-})
-
-router.post('/:id/comment',(req,res)=>{
-    console.log(req.body)
-    // req.body.rant = req.body.rant ? true :false
-    req.body.rant = !!req.body.rant             //returns boolean
-    db.Place.findById(req.params.id)
-    .then(place =>{
-        db.Comment.create(req.body)
-        .then(comment=>{
-            place.comments.push(comment.id)
-            place.save()
-            .then(()=>{
-                res.redirect(`/places/${req.params.id}`)
-            })
+router.post("/:id/comment", (req, res) => {
+  console.log(req.body);
+  // req.body.rant = req.body.rant ? true :false
+  req.body.rant = !!req.body.rant; //returns boolean
+  db.Place.findById(req.params.id)
+    .then((place) => {
+      db.Comment.create(req.body)
+        .then((comment) => {
+          place.comments.push(comment.id);
+          place.save().then(() => {
+            res.redirect(`/places/${req.params.id}`);
+          });
         })
-        .catch(err=>{
-            res.render('error404')
-        })
+        .catch((err) => {
+          res.render("error404");
+        });
     })
-    .catch(err=>{
-        res.render('error404')
-    })
-})
+    .catch((err) => {
+      res.render("error404");
+    });
+});
 
-router.delete('/:id/comment/:rantId',(req,res)=>{
-    res.send('Delete a rant about particular place')
-})
+router.delete("/:id/comment/:rantId", (req, res) => {});
 
-router.get('*',(req,res)=>{
-    res.render('places/error404')
-})
+router.get("*", (req, res) => {
+  res.render("places/error404");
+});
 
-
-module.exports = router
+module.exports = router;
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,92 +1,132 @@
-const React = require('react')
-const Def = require('../default')
+const React = require("react");
+const Def = require("../default");
 
 function show(data) {
-    let comments = (
-        <h3 className='inactive'>
-            No comments yet!
-        </h3>
-    )
-    if (data.place.comments.length) {
-        comments = data.place.comments.map(c => {
-            return (
-                <div key={c.id} className="border">
-                    <h2 className="rant">{c.rant ? 'Rant! 😡 ' : 'Rave! 😻'}</h2>
-                    <h5>{c.content}</h5>
-                    <h3>
-                        <strong>- {c.author}</strong>
-                    </h3>
-                    <h4>Rating: {c.stars}</h4>
-                </div>
-            )
-        })
-    }
-    return (
-        <Def>
-            <main>
-                <h1>{data.place.name}</h1>
-            </main>
-            <div className='showjsx-maincontainer'>
-                <div className='showjsx-img' >
-                    <img src={data.place.pic} alt='restaurant picture'></img>
-                </div>
-                <div className='showjsx-contentcontainer'>
-                    <div>
-                        <h2>Rating</h2>
-                        <p>Not rated yet! Be the first person!</p>
-                    </div>
-                    <div>
-                        <h2>Description</h2>
-                        <p>{data.place.showEstablished()}</p>
-                        <p>Serving {data.place.cuisines}</p>
-                    </div>
-                    <div className='showjsx-editndeletebox'>
-                        <div>
-                            <a href={`/places/${data.place.id}/edit`}>
-                                <i className="bi bi-pencil-fill"></i>
-                            </a>
-                        </div>
-                        <form method='POST' action={`/places/${data.place.id}?_method=DELETE`}>
-                            <button type="submit">
-                                <i className="bi bi-trash"></i>
-                            </button>
-                        </form>
-                    </div>
-                </div>
+  let comments = <h3 className="inactive">No comments yet!</h3>;
+  let rating = <h3 className="inactive">No ratings yet!</h3>;
+  if (data.place.comments.length) {
+    comments = data.place.comments.map((c) => {
+      return (
+        <div key={c.id} className="border">
+          <h2 className="rant">{c.rant ? "Rant! 😡 " : "Rave! 😻"}</h2>
+          <h5>{c.content}</h5>
+          <h3>
+            <strong>- {c.author}</strong>
+          </h3>
+          <h4>Rating: {c.stars}</h4>
+          <form
+            method="POST"
+            action={`/places/${data.place.id}/comment/${c.id}?_method=DELETE`}
+          >
+            <input
+              type="submit"
+              className="btn btn-danger"
+              value="Delete Comment"
+            />
+          </form>
+        </div>
+      );
+    });
+
+    const sumRatings = data.place.comments.reduce((total, { stars }) => {
+      return total + stars;
+    }, 0);
+    const averageRating = Math.round(sumRatings / data.place.comments.length);
+    // let stars = "";
+    // for (let i = 0; i < averageRating; i++) {
+    //   stars += "⭐️";
+    // }
+    const stars = new Array(averageRating).fill("⭐️");
+    rating = <h3>{stars}</h3>;
+  }
+  return (
+    <Def>
+      <main>
+        <h1>{data.place.name}</h1>
+      </main>
+      <div className="showjsx-maincontainer">
+        <div className="showjsx-img">
+          <img src={data.place.pic} alt="restaurant picture"></img>
+        </div>
+        <div className="showjsx-contentcontainer">
+          <div>
+            <h2>Rating</h2>
+            {rating}
+          </div>
+          <div>
+            <h2>Description</h2>
+            <p>{data.place.showEstablished()}</p>
+            <p>Serving {data.place.cuisines}</p>
+          </div>
+          <div className="showjsx-editndeletebox">
+            <div>
+              <a href={`/places/${data.place.id}/edit`}>
+                <i className="bi bi-pencil-fill"></i>
+              </a>
             </div>
-            <hr></hr>
-            <footer>
-                <form action={`/places/${data.place.id}/comment`} method='POST'>
-                    <div className='form-group'>
-                        <label htmlFor='author'>Author: </label>
-                        <input className='form-control' id='author' name='author' required/>
-                    </div>
-                    <div className='form-group'>
-                        <label htmlFor='content'>Content: </label>
-                        <textarea className='form-control' id='content' name='content' required/>
-                    </div>
-                    <div className='form-group'>
-                        <label htmlFor='stars'>Star Rating: </label>
-                        <input className='form-control' id='stars' name='stars' type='range' step='0.5' max='5' min='0' required/>
-                    </div>
-                    <div className='form-group'>
-                        <label htmlFor='rant'>Rant?: </label>
-                        <input className='form-control' id='rant' name='rant' type='checkbox' />
-                    </div>
-                    <button type='submit'>Add Comment</button>
-                </form>
-                <div className='showjsx-footer'>
-                    <h2>Comments</h2>
-                    <div className='showjsx-comments'>
-                        {comments}
-                    </div>
-                </div>
-            </footer>
-        </Def>
-    )
+            <form
+              method="POST"
+              action={`/places/${data.place.id}?_method=DELETE`}
+            >
+              <button type="submit">
+                <i className="bi bi-trash"></i>
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+      <hr></hr>
+      <footer>
+        <form action={`/places/${data.place.id}/comment`} method="POST">
+          <div className="form-group">
+            <label htmlFor="author">Author: </label>
+            <input
+              className="form-control"
+              id="author"
+              name="author"
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="content">Content: </label>
+            <textarea
+              className="form-control"
+              id="content"
+              name="content"
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="stars">Star Rating: </label>
+            <input
+              className="form-control"
+              id="stars"
+              name="stars"
+              type="range"
+              step="0.5"
+              max="5"
+              min="0"
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="rant">Rant?: </label>
+            <input
+              className="form-control"
+              id="rant"
+              name="rant"
+              type="checkbox"
+            />
+          </div>
+          <button type="submit">Add Comment</button>
+        </form>
+        <div className="showjsx-footer">
+          <h2>Comments</h2>
+          <div className="showjsx-comments">{comments}</div>
+        </div>
+      </footer>
+    </Def>
+  );
 }
 
-module.exports = show
-
-
-
+module.exports = show;
